693cd8d0a1fd5a1f6533a576da2b16392104b52c
kade042 <dereje88@gmail.com>
2016-02-11 23:56:20 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+#require './ttt-5-move-rb-q-000/lib/move.rb'
+def position_taken?(board,position)
+  if board[position] == " " || board[position] == ""
+    return false
+  elsif board[position] == "X" || board[position] == "O"
+    return true
+  else
+    return false
+  end
+end
+board = ["", " ", "", "", "", "", "","","X"]
+position = 0
+position_taken?(board, position)
+position = 8
+position_taken?(board, position)
